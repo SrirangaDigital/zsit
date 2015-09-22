3ab9daabef6c714dc1a9472987df3ffb83398fab
arjunkashyap <arjun.kashyap@yahoo.co.in>
2015-09-22 14:47:18 +0530

    Commit No. 37 - generate.sh file modified

--- generate.sh
+++ generate.sh
@@ -3,9 +3,9 @@
 host="localhost"
 db="zsi"
 usr="root"
-pwd="mysql"
+pwd='mysql'
 
-echo "drop database zsi; create database zsi;" | /usr/bin/mysql -uroot -pmysql
+echo "drop database if exists zsi; create database zsi;" | /usr/bin/mysql -u$usr -p$pwd
 
 perl author_records.pl $host $db $usr $pwd
 perl author_memoirs.pl $host $db $usr $pwd
@@ -92,17 +92,17 @@ perl searchtable_tcm.pl $host $db $usr $pwd
 perl searchtable_zlg.pl $host $db $usr $pwd
 perl searchtable_bulletin.pl $host $db $usr $pwd
 
-echo "create fulltext index text_index_records on searchtable_records (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_memoirs on searchtable_memoirs (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_occpapers on searchtable_occpapers (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_fbi on searchtable_fbi (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_fi on searchtable_fi (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_sfs on searchtable_sfs (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_cas on searchtable_cas (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_ess on searchtable_ess (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_hpg on searchtable_hpg (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_spb on searchtable_spb (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_sse on searchtable_sse (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_tcm on searchtable_tcm (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_zlg on searchtable_zlg (text);" | /usr/bin/mysql -uroot -pmysql zsi
-echo "create fulltext index text_index_bulletin on searchtable_bulletin (text);" | /usr/bin/mysql -uroot -pmysql zsi
+echo "create fulltext index text_index_records on searchtable_records (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_memoirs on searchtable_memoirs (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_occpapers on searchtable_occpapers (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_fbi on searchtable_fbi (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_fi on searchtable_fi (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_sfs on searchtable_sfs (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_cas on searchtable_cas (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_ess on searchtable_ess (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_hpg on searchtable_hpg (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_spb on searchtable_spb (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_sse on searchtable_sse (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_tcm on searchtable_tcm (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_zlg on searchtable_zlg (text);" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "create fulltext index text_index_bulletin on searchtable_bulletin (text);" | /usr/bin/mysql -u$usr -p$pwd $db
